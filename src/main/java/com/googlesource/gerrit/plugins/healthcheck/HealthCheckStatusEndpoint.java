41c2d7d1447f8b4dcd84b1d44a94399b26ed9e7a
Luca Milanesio <luca.milanesio@gmail.com>
2019-01-14 16:49:55 +0000

    Implement ReviewDb health-check
    Check if Gerrit is able to open a connection to ReviewDb
    and execute a simple query on it.
    
    Change-Id: I08184523670aad9f78dcbc2435424ec454ed2c5a

--- src/main/java/com/googlesource/gerrit/plugins/healthcheck/HealthCheckStatusEndpoint.java
+++ src/main/java/com/googlesource/gerrit/plugins/healthcheck/HealthCheckStatusEndpoint.java
@@ -1,18 +1,32 @@
 package com.googlesource.gerrit.plugins.healthcheck;
 
+import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.extensions.restapi.AuthException;
 import com.google.gerrit.extensions.restapi.BadRequestException;
 import com.google.gerrit.extensions.restapi.ResourceConflictException;
 import com.google.gerrit.extensions.restapi.RestReadView;
 import com.google.gerrit.server.config.ConfigResource;
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Singleton
 public class HealthCheckStatusEndpoint implements RestReadView<ConfigResource> {
 
+  private final DynamicSet<HealthCheck> healthChecks;
+
+  @Inject
+  public HealthCheckStatusEndpoint(DynamicSet<HealthCheck> healthChecks) {
+    this.healthChecks = healthChecks;
+  }
+
   @Override
   public Object apply(ConfigResource resource)
       throws AuthException, BadRequestException, ResourceConflictException, Exception {
-    return "{}";
+    Iterable<HealthCheck> iterable = () -> healthChecks.iterator();
+    return StreamSupport.stream(iterable.spliterator(), true)
+        .map(check -> check.run())
+        .collect(Collectors.toMap(r -> r.name, r -> r.result));
   }
 }

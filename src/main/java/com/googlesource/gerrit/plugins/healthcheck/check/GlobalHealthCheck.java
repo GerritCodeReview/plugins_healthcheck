837b5fd8989a7ed8c0db3735015d354fa79a281c
Luca Milanesio <luca.milanesio@gmail.com>
2019-01-14 16:49:55 +0000

    Implement ReviewDb health-check
    Check if Gerrit is able to open a connection to ReviewDb
    and execute a simple query on it.
    
    Change-Id: I08184523670aad9f78dcbc2435424ec454ed2c5a

---
+++ src/main/java/com/googlesource/gerrit/plugins/healthcheck/check/GlobalHealthCheck.java
@@ -0,0 +1,39 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.healthcheck.check;
+
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.inject.Inject;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+public class GlobalHealthCheck {
+
+  private final DynamicSet<HealthCheck> healthChecks;
+
+  @Inject
+  public GlobalHealthCheck(DynamicSet<HealthCheck> healthChecks) {
+    this.healthChecks = healthChecks;
+  }
+
+  public Map run() {
+    Iterable<HealthCheck> iterable = () -> healthChecks.iterator();
+    return StreamSupport.stream(iterable.spliterator(), false)
+        .map(check -> Arrays.asList(check.name(), check.run()))
+        .collect(Collectors.toMap(k -> k.get(0), v -> v.get(1)));
+  }
+}

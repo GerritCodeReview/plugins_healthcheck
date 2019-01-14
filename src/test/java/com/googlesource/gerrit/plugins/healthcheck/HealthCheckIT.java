4d3401d1af6425d881522cdd26b6c2ce4551cdb1
Luca Milanesio <luca.milanesio@gmail.com>
2019-01-14 16:49:55 +0000

    Implement ReviewDb health-check
    Check if Gerrit is able to open a connection to ReviewDb
    and execute a simple query on it.
    
    Change-Id: I08184523670aad9f78dcbc2435424ec454ed2c5a

--- src/test/java/com/googlesource/gerrit/plugins/healthcheck/HealthCheckIT.java
+++ src/test/java/com/googlesource/gerrit/plugins/healthcheck/HealthCheckIT.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2018 The Android Open Source Project
+// Copyright (C) 2019 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,18 +16,25 @@ package com.googlesource.gerrit.plugins.healthcheck;
 
 import static com.google.common.net.HttpHeaders.CONTENT_TYPE;
 import static com.google.common.truth.Truth.assertThat;
+import static com.googlesource.gerrit.plugins.healthcheck.check.HealthCheckNames.REVIEWDB;
 
 import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
 import com.google.gerrit.acceptance.RestResponse;
 import com.google.gerrit.acceptance.TestPlugin;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gwtorm.server.SchemaFactory;
+import com.google.inject.Inject;
 import java.io.IOException;
 import org.junit.Test;
 
-@TestPlugin(
-    name = "healthcheck",
-    sysModule = "com.googlesource.gerrit.plugins.healthcheck.Module",
-    httpModule = "com.googlesource.gerrit.plugins.healthcheck.HttpModule")
-public class HealthCheckTest extends LightweightPluginDaemonTest {
+@TestPlugin(name = "healthcheck", sysModule = "com.googlesource.gerrit.plugins.healthcheck.Module")
+public class HealthCheckIT extends LightweightPluginDaemonTest {
+  Gson gson = new Gson();
+
+  @Inject private SchemaFactory<ReviewDb> reviewDbSchemaFactory;
 
   @Test
   public void shouldReturnOkWhenHealthy() throws Exception {
@@ -37,6 +44,18 @@ public class HealthCheckTest extends LightweightPluginDaemonTest {
   @Test
   public void shouldReturnAJsonPayload() throws Exception {
     assertThat(getHealthCheckStatus().getHeader(CONTENT_TYPE)).contains("application/json");
+  }
+
+  @Test
+  public void shouldReturnReviewDbCheck() throws Exception {
+    RestResponse resp = getHealthCheckStatus();
+    resp.assertOK();
+
+    JsonObject respPayload = gson.fromJson(resp.getReader(), JsonObject.class);
+    assertThat(respPayload.has(REVIEWDB)).isTrue();
+
+    JsonPrimitive reviewDbStatus = respPayload.get(REVIEWDB).getAsJsonPrimitive();
+    assertThat(reviewDbStatus).isEqualTo(new JsonPrimitive(true));
   }
 
   private RestResponse getHealthCheckStatus() throws IOException {

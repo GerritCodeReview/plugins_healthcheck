b097fded50267e7b9ecd85a823e33bc485dbeb93
Luca Milanesio <luca.milanesio@gmail.com>
2019-01-14 15:55:19 +0000

    Scaffolding of the healthcheck API and build
    Define the overall structure of the health check API,
    with its return code and expected payload.
    
    Add the overall Bazel build and acceptance tests.
    
    Change-Id: Ifc25fcfa97c1fb4824e472349b8ae435e0f9551b

---
+++ src/test/java/com/googlesource/gerrit/plugins/healthcheck/HealthCheckTest.java
@@ -0,0 +1,45 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.healthcheck;
+
+import static com.google.common.net.HttpHeaders.CONTENT_TYPE;
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
+import com.google.gerrit.acceptance.RestResponse;
+import com.google.gerrit.acceptance.TestPlugin;
+import java.io.IOException;
+import org.junit.Test;
+
+@TestPlugin(
+    name = "healthcheck",
+    sysModule = "com.googlesource.gerrit.plugins.healthcheck.Module",
+    httpModule = "com.googlesource.gerrit.plugins.healthcheck.HttpModule")
+public class HealthCheckTest extends LightweightPluginDaemonTest {
+
+  @Test
+  public void shouldReturnOkWhenHealthy() throws Exception {
+    getHealthCheckStatus().assertOK();
+  }
+
+  @Test
+  public void shouldReturnAJsonPayload() throws Exception {
+    assertThat(getHealthCheckStatus().getHeader(CONTENT_TYPE)).contains("application/json");
+  }
+
+  private RestResponse getHealthCheckStatus() throws IOException {
+    return adminRestSession.get("/config/server/healthcheck~status");
+  }
+}

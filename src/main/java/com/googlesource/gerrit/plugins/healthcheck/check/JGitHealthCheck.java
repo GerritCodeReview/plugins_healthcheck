0695b2bcc12dd77af1912183b6272c743b15cca2
Luca Milanesio <luca.milanesio@gmail.com>
2019-01-15 15:59:39 +0000

    Implement JGit health-check
    Check that JGit is able to open and read objects
    from All-Projects repository.
    
    Change-Id: Ibc7dc325b2f3529636b6ed8297bf3ca13269b444

---
+++ src/main/java/com/googlesource/gerrit/plugins/healthcheck/check/JGitHealthCheck.java
@@ -0,0 +1,51 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.healthcheck.check;
+
+import static com.googlesource.gerrit.plugins.healthcheck.check.HealthCheckNames.JGIT;
+
+import com.google.gerrit.server.config.AllProjectsName;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.inject.Inject;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JGitHealthCheck extends AbstractHealthCheck {
+  private static final Logger log = LoggerFactory.getLogger(JGitHealthCheck.class);
+
+  private final GitRepositoryManager repositoryManager;
+  private final AllProjectsName allProjectsName;
+
+  @Inject
+  public JGitHealthCheck(GitRepositoryManager repositoryManager, AllProjectsName allProjectsName) {
+    super(JGIT);
+    this.repositoryManager = repositoryManager;
+    this.allProjectsName = allProjectsName;
+  }
+
+  @Override
+  protected boolean doCheck() {
+    try (Repository allProjects = repositoryManager.openRepository(allProjectsName)) {
+      ObjectId headObj = allProjects.resolve("HEAD");
+      allProjects.open(headObj).getType();
+      return true;
+    } catch (Exception e) {
+      log.warn("Unable to open " + allProjectsName + " repository", e);
+      return false;
+    }
+  }
+}

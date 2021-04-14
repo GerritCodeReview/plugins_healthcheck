6c6f1f17df2e35b9940c131d3d9c637af50f309b
Fabio Ponciroli <ponch78@gmail.com>
2021-04-14 15:18:39 +0200

    Plugin reports wrong number of failures
    The increment of the metric failure should happen
    *when* a new failure is happening and NOT when the metric
    is read from a callback.
    
    The metrics are now published when a check is triggered.
    Prometheus will just scrape them when needed without retriggering any
    calculation of the metrics.
    
    The metrics are now injected in the AbstractHealthCheck, which
    takes care of updating them whenever a check runs.
    
    Bug: Issue 14385
    Change-Id: I5f149f6893289c2874d125a9c04862d53cacd19b

---
+++ src/test/java/com/googlesource/gerrit/plugins/healthcheck/HealthCheckMetricsFactoryMock.java
@@ -0,0 +1,12 @@
+package com.googlesource.gerrit.plugins.healthcheck;
+
+import com.google.gerrit.metrics.DisabledMetricMaker;
+import com.google.gerrit.metrics.MetricMaker;
+
+public class HealthCheckMetricsFactoryMock implements HealthCheckMetricsFactory {
+  @Override
+  public HealthCheckMetrics create(String name) {
+    MetricMaker metricMaker = new DisabledMetricMaker();
+    return new HealthCheckMetrics(metricMaker, name);
+  }
+}

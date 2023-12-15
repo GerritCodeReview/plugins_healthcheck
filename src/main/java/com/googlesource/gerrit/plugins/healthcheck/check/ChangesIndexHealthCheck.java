bc3bd13d3ee8d85884e36bcaa79c183bde536449
Jacek Centkowski <geminica.programs@gmail.com>
2023-12-15 10:52:14 +0100

    Implement changes indexes lock files check
    Detect the following failures of both open and closed changes indexes
    write.lock files:
    * write.lock file is missing
    * write.lock file is not writeable by Gerrit
    
    Notes:
    * so far only changes indexes are subject of check
    * in order to test it `UseLocalDisk` annotation has to be applied (and
      it is heavy operation) therefore `HealthCheckIT` was modified so that
      `changesindex` healthcheck is disabled for all test cases (or
      additionally disabled in case when some checks get disabled)
    * `changesindex` healthcheck test cases were added to a dedicated
      `ChangesIndexHealthCheckIT` file
    * common functions from `HealthCheckIT` and `ChangesIndexHealthCheckIT`
      were introduced in `AbstractHealthCheckIntegrationTest`
    
    The write.lock ownership change detection was tested manually with the
    following steps:
    * docker run --rm -p 8080:8080 -p 29418:29418 -ti --name gerrit_3.9.1 gerritcodereview/gerrit:3.9.1
    * docker cp bazel-bin/plugins/healthcheck/healthcheck.jar gerrit_3.9.1:/var/gerrit/plugins
    * curl localhost:8080/config/server/healthcheck~status returned
      "changesindex": {
        "result": "passed",
        "ts": 1703147883563,
        "elapsed": 0
      }
    * docker exec -u root -it gerrit_3.9.1 /bin/bash
    * chown root:root /var/gerrit/index/changes_0084/open/write.lock
    * curl localhost:8080/config/server/healthcheck~status returned
      "changesindex": {
        "result": "failed",
        "ts": 1703148022276,
        "elapsed": 0
      }
    
    IOW it works as expected.
    
    Bug: Issue 40015289
    Change-Id: I66a4053483e2c10eb28c150782c33cfd10dc2b15

--- src/main/java/com/googlesource/gerrit/plugins/healthcheck/check/ChangesIndexHealthCheck.java
+++ src/main/java/com/googlesource/gerrit/plugins/healthcheck/check/ChangesIndexHealthCheck.java
@@ -20,32 +20,46 @@ import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.gerrit.index.IndexType;
 import com.google.gerrit.metrics.MetricMaker;
 import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.gerrit.server.config.SitePaths;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.googlesource.gerrit.plugins.healthcheck.HealthCheckConfig;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+import org.eclipse.jgit.errors.ConfigInvalidException;
 import org.eclipse.jgit.lib.Config;
+import org.eclipse.jgit.storage.file.FileBasedConfig;
+import org.eclipse.jgit.util.FS;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Singleton
 public class ChangesIndexHealthCheck extends AbstractHealthCheck {
   private static final Logger log = LoggerFactory.getLogger(QueryChangesHealthCheck.class);
+  private static final String lockFilename = "write.lock";
 
   private final boolean isLuceneIndex;
+  private final Optional<ChangesIndexLockFiles> changes;
 
   @Inject
   ChangesIndexHealthCheck(
       @GerritServerConfig Config cfg,
       ListeningExecutorService executor,
       HealthCheckConfig config,
-      MetricMaker metricMaker) {
+      MetricMaker metricMaker,
+      SitePaths sitePaths) {
     super(executor, config, CHANGES_INDEX, metricMaker);
     this.isLuceneIndex = isIndexTypeLucene(cfg);
+    this.changes = getChangesIndexLockFiles(sitePaths.index_dir);
   }
 
   @Override
   protected Result doCheck() throws Exception {
-    return isLuceneIndex ? Result.PASSED : Result.DISABLED;
+    return isLuceneIndex
+        ? changes.map(locks -> locks.valid() ? Result.PASSED : Result.FAILED).orElse(Result.FAILED)
+        : Result.DISABLED;
   }
 
   private static boolean isIndexTypeLucene(Config cfg) {
@@ -57,5 +71,53 @@ public class ChangesIndexHealthCheck extends AbstractHealthCheck {
           indexType);
     }
     return isLucene;
+  }
+
+  private static Optional<ChangesIndexLockFiles> getChangesIndexLockFiles(Path indexDir) {
+    FileBasedConfig cfg =
+        new FileBasedConfig(indexDir.resolve("gerrit_index.config").toFile(), FS.detect());
+    try {
+      cfg.load();
+      return getActiveIndexVersion(cfg, "changes")
+          .map(version -> getChangesLockFiles(indexDir, version));
+    } catch (IOException | ConfigInvalidException e) {
+      log.error("Getting changes index version from configuration failed", e);
+    }
+    return Optional.empty();
+  }
+
+  private static Optional<String> getActiveIndexVersion(Config cfg, String indexPrefix) {
+    String section = "index";
+    return cfg.getSubsections(section).stream()
+        .filter(
+            subsection ->
+                subsection.startsWith(indexPrefix)
+                    && cfg.getBoolean(section, subsection, "ready", false))
+        .findAny();
+  }
+
+  private static ChangesIndexLockFiles getChangesLockFiles(Path indexDir, String indexVersion) {
+    Path versionDir = indexDir.resolve(indexVersion);
+    return new ChangesIndexLockFiles(
+        versionDir.resolve("open").resolve(lockFilename).toFile(),
+        versionDir.resolve("closed").resolve(lockFilename).toFile());
+  }
+
+  private static class ChangesIndexLockFiles {
+    private final File openLock;
+    private final File closedLock;
+
+    private ChangesIndexLockFiles(File openLock, File closedLock) {
+      this.openLock = openLock;
+      this.closedLock = closedLock;
+    }
+
+    private boolean valid() {
+      return validLock(openLock) && validLock(closedLock);
+    }
+
+    private static boolean validLock(File writeLock) {
+      return writeLock.isFile() && writeLock.canWrite();
+    }
   }
 }

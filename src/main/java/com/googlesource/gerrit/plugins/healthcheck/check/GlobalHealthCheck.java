e154e0a0b438930f0be6acd308320d1d1c2e62ac
Luca Milanesio <luca.milanesio@gmail.com>
2024-05-05 21:37:37 +0100

    Short-circuit on failed healthchecks
    Before the full set of checks was always executed
    even if it was clear that it would have failed
    because of one of the checks failing.
    
    Avoid running the additional checks when one
    of the checks is failing for any reason and
    therefore running the full set would not be
    useful.
    
    Introduce the new status NOT_RUN that clarifies
    that the check was not executed because it would
    have not change the overall status.
    
    This change allows a Gerrit to avoid running
    extra checks when the system is already struggling.
    Having too may healthchecks could actually prevent
    the system to become healthy again. Minimizing
    the execution of checks on a struggling system
    is paramount for allowing it to recover more
    quickly.
    
    Change-Id: I885b3df06b60e322f12b77674be02536e8131499

--- src/main/java/com/googlesource/gerrit/plugins/healthcheck/check/GlobalHealthCheck.java
+++ src/main/java/com/googlesource/gerrit/plugins/healthcheck/check/GlobalHealthCheck.java
@@ -22,8 +22,11 @@ import com.google.gerrit.metrics.MetricMaker;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.googlesource.gerrit.plugins.healthcheck.HealthCheckConfig;
+import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -31,6 +34,31 @@ import java.util.stream.StreamSupport;
 public class GlobalHealthCheck extends AbstractHealthCheck {
 
   private final DynamicSet<HealthCheck> healthChecks;
+
+  public static class MemoizedStatusSummary implements Supplier<StatusSummary> {
+    private final AtomicReference<StatusSummary> result = new AtomicReference<>();
+    private final HealthCheck check;
+
+    MemoizedStatusSummary(HealthCheck check) {
+      this.check = check;
+    }
+
+    @Override
+    public StatusSummary get() {
+      if (result.get() == null) {
+        result.set(check.run());
+      }
+      return result.get();
+    }
+
+    public StatusSummary getIfCompleted() {
+      StatusSummary completedResult = result.get();
+      return completedResult == null
+          ? new StatusSummary(
+              Result.NOT_RUN, System.currentTimeMillis(), 0L, Collections.emptyMap())
+          : completedResult;
+    }
+  }
 
   @Inject
   public GlobalHealthCheck(
@@ -46,17 +74,16 @@ public class GlobalHealthCheck extends AbstractHealthCheck {
   public HealthCheck.StatusSummary run() {
     Iterable<HealthCheck> iterable = () -> healthChecks.iterator();
     long ts = System.currentTimeMillis();
-    Map<String, Object> checkToResults =
+    Map<String, MemoizedStatusSummary> checkToResults =
         StreamSupport.stream(iterable.spliterator(), false)
-            .parallel()
-            .collect(Collectors.toMap(HealthCheck::name, HealthCheck::run));
+            .collect(Collectors.toMap(HealthCheck::name, MemoizedStatusSummary::new));
     long elapsed = System.currentTimeMillis() - ts;
+    Result checkResult = hasAnyFailureOnResults(checkToResults) ? Result.FAILED : Result.PASSED;
+    Map<String, Object> reportedResults =
+        checkToResults.entrySet().stream()
+            .collect(Collectors.toMap(Map.Entry::getKey, v -> v.getValue().getIfCompleted()));
     StatusSummary globalStatus =
-        new HealthCheck.StatusSummary(
-            hasAnyFailureOnResults(checkToResults) ? Result.FAILED : Result.PASSED,
-            ts,
-            elapsed,
-            checkToResults);
+        new HealthCheck.StatusSummary(checkResult, ts, elapsed, reportedResults);
     if (globalStatus.isFailure()) {
       failureCounterMetric.increment();
     }
@@ -69,11 +96,7 @@ public class GlobalHealthCheck extends AbstractHealthCheck {
     return run().result;
   }
 
-  public static boolean hasAnyFailureOnResults(Map<String, Object> results) {
-    return results.values().stream()
-        .anyMatch(
-            res ->
-                res instanceof StatusSummary
-                    && ((StatusSummary) res).isFailure());
+  public static boolean hasAnyFailureOnResults(Map<String, MemoizedStatusSummary> results) {
+    return results.values().stream().parallel().anyMatch(res -> res.get().isFailure());
   }
 }

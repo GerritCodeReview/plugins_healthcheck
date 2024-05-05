8fce7b27e19ecad6f03f5a6fdce7d394e02961f5
Luca Milanesio <luca.milanesio@gmail.com>
2024-05-05 21:37:37 +0100

    Short-circuit on failed healthchecks
    Before the full set of checks was always executed even if it was clear
    that it would have failed because of one of the checks failing.
    
    Avoid running the additional checks when one of the checks is failing
    for any reason and therefore running the full set would not be useful.
    
    Introduce the new status NOT_RUN that clarifies that the check was
    not executed because it would have not change the overall status.
    
    This change allows a Gerrit to avoid running extra checks when the
    system is already struggling. Having too may healthchecks could
    actually prevent the system to become healthy again.
    Minimizing the execution of checks on a struggling system is paramount
    for allowing it to recover more quickly.
    
    Change-Id: I885b3df06b60e322f12b77674be02536e8131499

--- src/test/java/com/googlesource/gerrit/plugins/healthcheck/AbstractHealthCheckIntegrationTest.java
+++ src/test/java/com/googlesource/gerrit/plugins/healthcheck/AbstractHealthCheckIntegrationTest.java
@@ -16,13 +16,20 @@ package com.googlesource.gerrit.plugins.healthcheck;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
 import com.google.gerrit.acceptance.RestResponse;
 import com.google.gerrit.extensions.annotations.PluginName;
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.metrics.MetricMaker;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
 import com.google.inject.Key;
+import com.google.inject.Singleton;
+import com.googlesource.gerrit.plugins.healthcheck.check.AbstractHealthCheck;
+import com.googlesource.gerrit.plugins.healthcheck.check.HealthCheck;
 import com.googlesource.gerrit.plugins.healthcheck.check.HealthCheckNames;
 import java.io.IOException;
 import org.eclipse.jgit.errors.ConfigInvalidException;
@@ -34,6 +41,36 @@ class AbstractHealthCheckIntegrationTest extends LightweightPluginDaemonTest {
     protected void configure() {
       install(new HealthCheckExtensionApiModule());
       install(new Module());
+      DynamicSet.bind(binder(), HealthCheck.class).to(FakeHealthCheck.class);
+    }
+  }
+
+  @Singleton
+  public static class FakeHealthCheck extends AbstractHealthCheck {
+    private long sleep = 0L;
+    private Result result = Result.PASSED;
+
+    @Inject
+    public FakeHealthCheck(
+        ListeningExecutorService executor,
+        HealthCheckConfig config,
+        String name,
+        MetricMaker metricMaker) {
+      super(executor, config, "fake-check", metricMaker);
+    }
+
+    public void setSleep(long sleep) {
+      this.sleep = sleep;
+    }
+
+    public void setResult(Result result) {
+      this.result = result;
+    }
+
+    @Override
+    protected Result doCheck() throws Exception {
+      Thread.sleep(sleep);
+      return result;
     }
   }
 
